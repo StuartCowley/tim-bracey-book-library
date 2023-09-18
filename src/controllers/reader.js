e14adfc5b1995fe6e92c70a4a3ad1eb212f5def0
Tim <50870607+toothless13@users.noreply.github.com>
2023-09-18 21:39:03 +0100

    start implementing getReader route

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -5,4 +5,13 @@ exports.createReader = async (req, res) => {
   res.status(201).json(newReader);
 }
 
+exports.getAllReaders = async (_, res) => {
+  const readers = await Reader.findAll();
+  res.status(200).json(readers);
+}
+
+exports.getReader = async (req, res) => {
+
+}
+
 // module.exports = { createReader }
--- src/routes/reader.js
+++ src/routes/reader.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createReader } = require('../controllers/reader');
+const { createReader, getAllReaders } = require('../controllers/reader');
 const reader = require('../models/reader');
 
 const readerRouter = express.Router();
 
 readerRouter.post('/', createReader);
+
+readerRouter.get('/', getAllReaders);
 
 module.exports = readerRouter;
