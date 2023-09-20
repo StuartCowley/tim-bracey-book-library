1dd5a1bc890b02c1047fa987a9029136ee6a028c
Tim <50870607+toothless13@users.noreply.github.com>
2023-09-20 15:25:34 +0100

    implement no null values, and email and password contraints and validations for model

--- src/models/reader.js
+++ src/models/reader.js
@@ -1,8 +1,26 @@
 module.exports = (connection, DataTypes) => {
   const schema = {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [8],
+          msg: "Password must be 8 characters or longer in length",
+        },
+      }
+    }
   };
 
   const ReaderModel = connection.define('Reader', schema);
