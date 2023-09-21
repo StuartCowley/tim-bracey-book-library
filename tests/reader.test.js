1d2261532f1d946abfac8c5ed0d8a7a08df66367
Tim <50870607+toothless13@users.noreply.github.com>
2023-09-21 14:18:59 +0100

    add tests for password removal return

--- tests/reader.test.js
+++ tests/reader.test.js
@@ -24,6 +24,7 @@ describe('/readers', () => {
 
         expect(response.status).to.equal(201);
         expect(response.body.name).to.equal('Elizabeth Bennet');
+        expect(response.body.password).to.not.exist;
         expect(newReaderRecord.name).to.equal('Elizabeth Bennet');
         expect(newReaderRecord.email).to.equal('future_ms_darcy@gmail.com');
         expect(newReaderRecord.password).to.equal('password1');
@@ -65,7 +66,8 @@ describe('/readers', () => {
           const expected = readers.find((a) => a.id === reader.id);
 
           expect(reader.name).to.equal(expected.name);
-          expect(reader.email).to.equal(expected.email);
+          expect(reader.email).to.equal(expected.email)
+          expect(reader.password).to.not.exist;
         });
       });
     });
@@ -78,6 +80,7 @@ describe('/readers', () => {
         expect(response.status).to.equal(200);
         expect(response.body.name).to.equal(reader.name);
         expect(response.body.email).to.equal(reader.email);
+        expect(response.body.password).to.not.exist;
       });
 
       it('returns a 404 if the reader does not exist', async () => {
