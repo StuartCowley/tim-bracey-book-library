1ed2d7ec147ff887d0367778178c193d9f1a4d44
Tim <50870607+toothless13@users.noreply.github.com>
2023-09-21 10:43:33 +0100

    add unsaved files that remove old functions

--- src/routes/reader.js
+++ src/routes/reader.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const { createReader, getAllReaders, getReader, updateReader, deleteReader } = require('../controllers/reader');
 const { createItem } = require('../controllers/helper');
-// const reader = require('../models/reader');
 const { Reader } = require('../models');
 
 const readerRouter = express.Router();
-
-// readerRouter.post('/', createReader);
 
 readerRouter.post('/', createItem(Reader));
 
